7400b03bfaff8e0cd4a926d6431d36a11661d545
Patrick Mahloy <patrick.mahloy@gmail.com>
2022-04-12 00:05:15 -0400

    added down payment form input, started implementing flexbox, looking at chart.js for a doughnut chart

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-let loanAmount;
+let homeValue;
+let downPayment;
+let financedAmount;
 let term;
 let apr;
 let payment;
@@ -8,13 +10,15 @@ window.onload = function() {
 };
 
 function getValues() {
+  homeValue = document.getElementById("home-value").value;
+  downPayment = document.getElementById("down-payment").value;
   term = document.getElementById("term").value;
   apr = document.getElementById("apr").value;
-  loanAmount = document.getElementById("amount").value;
 
+  financedAmount = homeValue - downPayment;
   apr /= 1200;
   term *= 12;
 
-  payment = loanAmount*(apr * Math.pow((1 + apr), term))/(Math.pow((1 + apr), term) - 1);
+  payment = financedAmount*(apr * Math.pow((1 + apr), term))/(Math.pow((1 + apr), term) - 1);
   document.getElementById("payment").value = "$" + payment.toFixed(2) + " per month";
 };

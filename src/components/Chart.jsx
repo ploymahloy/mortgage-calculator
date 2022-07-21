0727b2d6d9ff18cdec336ad513f122c388541d1e
Patrick Mahloy <patrick.mahloy@gmail.com>
2022-07-21 19:50:48 -0400

    chart reflects data as intended

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -7,38 +7,39 @@ import './Chart.css';
 export default function Chart({ chartData }) {
 	const { homeValue, downPayment, term, apr } = chartData;
 
-  ChartJS.register(ArcElement, Title, Tooltip, Legend);
-  
-  // 1) Principal (P): homeValue - downPayment
-  // 2) Interest Rate (i): (apr / 100) * 12
-  // 3) Number of Payments (n): term * 12
-  // 4) PMI: Principal * .01
-  // 5) Property Tax: homeValue * .01
-  // 6) House Insurance: (homeValue / 1000) * 3/5
-  // 7) Monthly Payment (M): 
+	ChartJS.register(ArcElement, Title, Tooltip, Legend);
 
 	const amt = homeValue - downPayment;
-	const i = +apr * 12;
+	const i = apr / 1200;
 	const months = term * 12;
-  const M = amt*(i * Math.pow((1 + i), months))/(Math.pow((1 + i), months) - 1);
-  
-  console.log(i);
+
+	const PandI = Math.round(
+		(amt * (i * Math.pow(1 + i, months))) / (Math.pow(1 + i, months) - 1)
+	);
+	const propertyTax = (homeValue * 0.009) / 12;
+	const insurance = ((homeValue / 1000) * 3) / 5;
+
+	const paymentAmount = Math.round(
+		(amt * (i * Math.pow(1 + i, months))) / (Math.pow(1 + i, months) - 1) +
+			propertyTax +
+			insurance
+	);
+
+	console.log(paymentAmount);
 
 	const data = {
-		labels: ['P&I', 'Tax', 'Insurance', 'PMI'],
+		labels: ['P&I', 'Tax', 'Insurance'],
 		datasets: [
 			{
-				data: [M, 300, 76, 307],
+				data: [PandI, propertyTax, insurance],
 				backgroundColor: [
 					'rgba(0, 136, 79, 0.7)',
 					'rgba(42, 157, 143, 0.7)',
-					'rgba(83, 187, 151, 0.7)',
 					'rgba(130, 215, 154, 0.7)'
 				],
 				borderColor: [
 					'rgba(0, 136, 79, 1)',
 					'rgba(42, 157, 143, 1)',
-					'rgba(83, 187, 151, 1)',
 					'rgba(130, 215, 154, 1)'
 				],
 				borderWidth: 3
@@ -48,7 +49,8 @@ export default function Chart({ chartData }) {
 
 	return (
 		<div className="chart-wrapper">
-      <Doughnut data={data} height={'90px'} width={'90px'} id="chart" />
+			<Doughnut data={data} height={'90px'} width={'90px'} id="chart" />
+			<div>Monthly Payment: ${paymentAmount}</div>
 		</div>
 	);
 }
